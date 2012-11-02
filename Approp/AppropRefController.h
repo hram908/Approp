9af7f9cc76ff2d3c80d5caa5b56a4218fd934ff3
dianna <diannamertz@gmail.com>
2012-11-02 20:51:32 +0100

    Have two working Ref Controllers

---
+++ Approp/AppropRefController.h
@@ -0,0 +1,15 @@
+//
+//  AppropRefController.h
+//  Approp
+//
+//  Created by Dianna Mertz on 11/2/12.
+//  Copyright (c) 2012 Dianna Mertz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppropRefController : UITableViewController <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, retain) NSArray *paintingsArray;
+
+@end
